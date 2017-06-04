b19ed2784d08f12545874a8388bcb5159583e82e
assis <assis_fest@live.it>
2017-06-04 10:06:17 -0700

    Quebrando hash por força bruta.

---
+++ src/br/assis/ufpi/seguranca/quebrahash/QuebraSenha.java
@@ -0,0 +1,27 @@
+package br.assis.ufpi.seguranca.quebrahash;
+
+import com.google.common.base.Charsets;
+import com.google.common.hash.Hashing;
+
+public class QuebraSenha {
+	
+	public String quebrarSenha(String stringMatriculaNome, String hash){
+		String[] matriculaComNome = stringMatriculaNome.split("");
+		String senhaQuebrada = "";
+		for (int i = 0; i < matriculaComNome.length; i++) {
+			for (int j = 0; j < matriculaComNome.length; j++) {
+				for (int j2 = 0; j2 < matriculaComNome.length; j2++) {
+					for (int k = 0; k < matriculaComNome.length; k++) {
+						senhaQuebrada = "";
+						senhaQuebrada += matriculaComNome[i] + matriculaComNome[j] + matriculaComNome[j2] + matriculaComNome[k];
+						if(Hashing.sha1().hashString(senhaQuebrada, Charsets.UTF_8).toString().equals(hash)){
+							i=100;j=100;k=100;j2=100;
+						}
+					}
+				}
+			}
+		}
+		return senhaQuebrada;
+	}
+
+}
